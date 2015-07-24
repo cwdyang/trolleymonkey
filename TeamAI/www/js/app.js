945f28906942f12ce7a89fe87f8c13c7980ca195
Dirgha Hermanta <dirghah@datacom.co.nz>
2015-07-24 23:30:12 +1200

    Fixed Settings page

--- TeamAI/www/js/app.js
+++ TeamAI/www/js/app.js
@@ -77,25 +77,26 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       })
 
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+  //  .state('app.playlists', {
+  //    url: '/playlists',
+  //    views: {
+  //      'menuContent': {
+  //        templateUrl: 'templates/playlists.html',
+  //        controller: 'PlaylistsCtrl'
+  //      }
+  //    }
+  //  })
+  //
+  //.state('app.single', {
+  //  url: '/playlists/:playlistId',
+  //  views: {
+  //    'menuContent': {
+  //      templateUrl: 'templates/playlist.html',
+  //      controller: 'PlaylistCtrl'
+  //    }
+  //  }
+  //}
+  //);
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/shoppinglist');
 });
