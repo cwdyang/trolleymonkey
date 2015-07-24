27cf3762db897817b859a30a89b187549851cf1f
unknown <TimurK@TimurK-LT001.datacom.net.nz>
2015-07-25 11:08:35 +1200

    EF settings access + fixed web.api reference

---
+++ WebApplication4/DataAccess/Repos/SettingsRepo.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Models;
+using DataAccess.ViewModels;
+
+namespace DataAccess.Repos
+{
+    public class SettingsRepo
+    {
+        private readonly IfoodstuffsEntities _context;
+
+        public SettingsRepo()
+        {
+            _context = new foodstuffsEntities();
+        }
+
+        public SettingsRepo(IfoodstuffsEntities context)
+        {
+            _context = context;
+        }
+
+
+        public Settings Get(int id)
+        {
+            var tbdcCustomerProfile = _context.tbdcCustomerProfiles.Find(id);
+            return new Settings(tbdcCustomerProfile);
+        }
+    }
+}
--- WebApplication4/WebApplication4/Controllers/SettingsController.cs
+++ WebApplication4/WebApplication4/Controllers/SettingsController.cs
@@ -4,36 +4,44 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using DataAccess.Repos;
+using DataAccess.ViewModels;
 
 namespace WebApplication4.Controllers
 {
     public class SettingsController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+
+        private readonly SettingsRepo _settingsRepo;
+
+        public SettingsController()
         {
-            return new string[] { "value1", "value2" };
+            _settingsRepo = new SettingsRepo();
         }
 
         // GET api/<controller>/5
-        public string Get(int id)
+        public Settings Get(int id)
         {
-            return "value";
+            var model = _settingsRepo.Get(id);
+            return model;
         }
 
         // POST api/<controller>
         public void Post([FromBody]string value)
         {
+            throw new NotImplementedException();
         }
 
         // PUT api/<controller>/5
         public void Put(int id, [FromBody]string value)
         {
+            throw new NotImplementedException();
         }
 
         // DELETE api/<controller>/5
         public void Delete(int id)
         {
+            throw new NotImplementedException();
         }
     }
 }
