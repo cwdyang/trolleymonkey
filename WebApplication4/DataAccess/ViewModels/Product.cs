60ff5ecb7c06eeef614b5fc12a9452b2663d5d1c
Aaron Small <discoaaron2@gmail.com>
2015-07-25 23:19:42 +1200

    New View Models

--- WebApplication4/DataAccess/ViewModels/Product.cs
+++ WebApplication4/DataAccess/ViewModels/Product.cs
@@ -1,22 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DataAccess.Models;
+﻿using System.Runtime.Serialization;
 
-namespace DataAccess.ViewModels
+[DataContract]
+public class Product
 {
-    public class Product
+    [DataMember]
+    public long ProductId { get; set; }
+
+    [DataMember]
+    public string ShortDescription { get; set; }
+
+    [DataMember]
+    public string ImageUrl { get; set; }
+
+    [DataMember]public string QuantityUnt { get; set; }
+
+    [DataMember]
+    public double PricePerUnit { get; set; }
+
+    public Product(long id, string descr, string url, string quantity, double price)
     {
+        ProductId = id;
+        ShortDescription = descr;
+        ImageUrl = url;
+        QuantityUnt = quantity;
+        PricePerUnit = price;
+    }
 
-        public Product(long id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
-
-        public long Id { get; set; }
-        public string Name { get; set; }
+    public Product(long id, string descr)
+    {
+        ProductId = id;
+        ShortDescription = descr;
+        ImageUrl = "http://www.hormelfoods.com/~/media/HormelFoods/Images/Brands/Product%20Shots/High%20Res%20Product%20Shots/spam-family-of-products.ashx";
+        QuantityUnt = "";
+        PricePerUnit = 69.00;
     }
 }
---
+++ WebApplication4/DataAccess/ViewModels/ShoppingListProduct.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+[DataContract]
+public class ShoppingListProduct
+{
+    [DataMember]
+    public int ShoppingListProductId { get; set; }
+
+    [DataMember]
+    public int Quantity { get; set; }
+
+    [DataMember]
+    public Product Product { get; set; }
+
+    public ShoppingListProduct(int id, int quantity, Product p)
+    {
+        ShoppingListProductId = id;
+        Quantity = quantity;
+        Product = p;
+    }
+
+}
